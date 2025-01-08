c69f37709085efaa92d25b21b64d643e9dd893f3
samuelconlon <conlonsp@yahoo.com>
2025-01-07 17:12:52 -1000

    commit tree 828c4282116f71d921350423c741bdc75b171e79

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('DOM Loaded')
+  const text = document.getElementById('text')
+  text.textContent = "This is really cool!"
+})
+
+console.log('Before DOM Loads')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   let p = document.getElementById('text')
+//   p.textContent = 'This is really cool!'
+// })
